546df3f7bd47d94a5090865e0149f3323d3e6517
JustynF <florendojustyn@gmail.com>
2018-03-05 12:26:34 -0500

    modified css styling and added new versions of webpages

---
+++ js/table.js
@@ -0,0 +1,25 @@
+
+(function ($) {
+	"use strict";
+	$('.column').on('mouseover',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
+
+		$(table2).find("."+column).addClass('hov-column-'+ verTable);
+		$(table1).find(".rows.head ."+column).addClass('hov-column-head-'+ verTable);
+	});
+
+	$('.column').on('mouseout',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
+
+		$(table2).find("."+column).removeClass('hov-column-'+ verTable);
+		$(table1).find(".rows.head ."+column).removeClass('hov-column-head-'+ verTable);
+	});
+    
+
+})(jQuery);
