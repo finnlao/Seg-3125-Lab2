ec32a16933fe9bc8d7e11899c6dec7ecac1c2636
JustynF <florendojustyn@gmail.com>
2018-03-05 12:29:49 -0500

    added additional files

--- js/main.js
+++ js/main.js
@@ -40,11 +40,12 @@
     });
 
      function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        if($(input).attr('type') == 'DOB' || $(input).attr('name') == 'DOB') {
+            if($(input).val().trim().match(/[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/) == null) {
                 return false;
             }
         }
+
         else {
             if($(input).val().trim() == ''){
                 return false;
